f37e40e9a73d9c090207f7260401493615bcc1a4
Kamrujjaman Md <jaman.edux@gmail.com>
2024-12-28 00:05:20 +0100

    Create utility for wind direction

--- src/utils/weatherUtils.ts
+++ src/utils/weatherUtils.ts
@@ -1,3 +1,13 @@
 export const convertTemperature = (temp: number, unit: 'C' | 'F') => {
-    return unit === 'F' ? Math.round((temp * 9) / 5 + 32) : Math.round(temp)
-  }
+  return unit === 'F' ? Math.round((temp * 9) / 5 + 32) : Math.round(temp)
+}
+
+export const getWindDirection = (degrees: number) => {
+  const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+  const index = Math.round((degrees % 360) / 45) % 8
+  return directions[index]
+}
+
+export const getWindRotation = (degrees: number) => {
+  return (degrees + 180) % 360
+}
